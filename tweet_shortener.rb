1c9281a2f61a97830311a2b4721866f2aa326c04
H B <phajib@gmail.com>
2019-01-29 07:42:14 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,17 @@
+def dictionary
+  substitute = {
+    "and" => "&",
+    "at" => "@",
+    "be" => "b",
+    "for" => "4",
+    "four" => "4",
+    "hello" => "hi",
+    "to" => "2",
+    "too" => "2",
+    "two" => "2",
+    "you" => "u"
+  }
+
 def word_substituter(tweet)
   repl_tweet = []
   tweet.each do |str|
